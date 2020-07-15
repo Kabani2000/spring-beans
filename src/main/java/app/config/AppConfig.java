d27a97cc23cac1a9351e5540baafddbf6cce61a5
Professional <avkabanov@gmail.com>
2020-07-15 13:23:44 +0300

    Пёсики

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,18 @@
 package app.config;
 
+import app.model.Dog;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+    @Scope("prototype")
+    @Bean(name="dog")
+    public Dog getDog() {
+        Dog dog = new Dog();
+        return dog;
+    }
 }
